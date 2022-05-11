3ed588f7b17d2cb43bd1c817e953b90b70ef3af3
Gabriel Mata <gabrielhvmata@gmail.com>
2022-05-10 23:27:55 -0300

    Remove GOOGLE CHROME BIN VARIABLE

--- app/classes/web_scraper.py
+++ app/classes/web_scraper.py
@@ -26,7 +26,7 @@ class WebScraper:
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         self.selenium_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options )
         # self.selenium_driver = webdriver.Chrome(executable_path=str(os.environ.get("CHROMEDRIVER_PATH")),options=chrome_options )
         self.url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"
