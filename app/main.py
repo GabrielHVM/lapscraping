57fba409bf8a9494893ff50437a1fd012b23f3d5
Gabriel Mata <gabrielhvmata@gmail.com>
2022-05-12 00:05:34 -0300

    CORS and Documentation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 """Module with main code"""
 from app.classes.web_scraper import WebScraper
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
-app = Flask(__name__)   
+app = Flask(__name__)
+CORS(app)   
 
 @app.route("/", methods=["GET"])
 def process_request():
